bff9f96e7a26480d090bb8bd8959d49cc9dec385
thewolfgames <thewolfgames@yahoo.com>
2021-07-15 22:33:43 +0300

    702s

--- ps4.js
+++ ps4.js
@@ -199,13 +199,13 @@ function toggle_payload(pld){
 		preloadScripts(['jb.js', 'preloader.js', 'lam132.js', 'loader.js']);
 	}else if(pld == "rd2113"){
 		document.getElementById("progress").innerHTML="Loading Payload.. Please wait..";
-		preloadScripts(['preloader.js', '702-Oysters-Free-1.13.js', 'loader.js']);
+		preloadScripts(['preloader.js', 'Oysters-1.13.js', 'loader.js']);
 	}else if(pld == "rd2114"){
 		document.getElementById("progress").innerHTML="Loading Payload.. Please wait..";
-		preloadScripts(['preloader.js', '702-Oysters-Free-1.19.js', 'loader.js']);
+		preloadScripts(['preloader.js', 'Oysters-1.19.js', 'loader.js']);
 	}else if(pld == "rd2124"){
 		document.getElementById("progress").innerHTML="Loading Payload.. Please wait..";
-		preloadScripts(['preloader.js', '702-Oysters-Free-1.24.js', 'loader.js']);
+		preloadScripts(['preloader.js', 'Oysters-1.24.js', 'loader.js']);
 	}
 	if(window.postPayload)
 		window.postPayload();
